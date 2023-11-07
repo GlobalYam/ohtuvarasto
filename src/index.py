081e6cbdb87ae30665510f7be9553fc7ff80212b
GlobalYam <aarni.hellen@gmail.com>
2023-11-07 18:45:28 +0200

    viikko 2 pylint

--- src/index.py
+++ src/index.py
@@ -1,19 +1,20 @@
 from varasto import Varasto
 
 
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def print_alkutila(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+
+def print_olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+
+def print_mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +23,8 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+
+def print_virhetilanteet():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,16 +34,22 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+
+def print_olutlaskenta(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+
+def print_mehulaskenta(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+
+def print_ota_varastosta(olutta, mehua):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
@@ -52,6 +61,18 @@ def main():
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    print_alkutila(mehua, olutta)
+    print_olut_getterit(olutta)
+    print_mehu_setterit(mehua)
+    print_virhetilanteet()
+    print_olutlaskenta(olutta)
+    print_ota_varastosta(olutta, mehua)
 
 
 if __name__ == "__main__":
